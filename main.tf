9955e481e5c478ccc3d3c62baeef66a046ca319b
Harit <malikharit016@gmail.com>
2026-03-11 11:44:06 +0530

    modified version tag

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "static_site" {
-  source = "git::https://github.com/Demon0067/tf-mod-s3-static-site.git?ref=v1.1"
+  source = "git::https://github.com/Demon0067/tf-mod-s3-static-site.git?ref=v1.2"
 
   bucket_name = "harit-static-site-demo-2026"
 }
